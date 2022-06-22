110ffe04858d64c765482d01ec03cf4479a72670
DmitriiKaban <dima16kravchenko@gmail.com>
2022-06-22 21:04:29 +0300

    Udd console app, edit git ignore

---
+++ ConsoleAppEFDBFirst/ConsoleAppEFDBFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEFDBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EF 6 Database First");
+
+            UseEF6();
+
+            Console.ReadKey();
+        }
+
+        private static void UseEF6()
+        {
+            
+        }
+    }
+}
